8eae9dcd15b87e25c14ce09248b0014319e9f340
iliaselboazzati <ilias-elboazzati@hotmail.com>
2021-06-01 00:16:07 +0200

    Exo2 DOM

---
+++ Js/main.js
@@ -0,0 +1,10 @@
+let h1 = document.createElement("h1");
+
+let h1Text = document.createTextNode("Run forest !");
+
+h1.appendChild(h1text);
+
+let myBody = document.querySelector("body");
+h1.append(h1);
+myBody.appendChild(h1);
+console.log(myBody);
